5340655cd6c2706e3aa76eae5e2f30b56d1f01fa
ivan0chen <youngfu.chen@gmail.com>
2018-07-21 09:34:20 +0800

    Chapter 9 finished

--- article/views.py
+++ article/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from article.models import Article,Comment
+from article.forms import ArticleForm
+from django.contrib import messages
+from django.db.models.query_utils import Q
 
 def article(request):
     articles = Article.objects.all()
@@ -10,3 +13,53 @@ def article(request):
         itemList.append(items)
     context = {'itemList': itemList}
     return render(request, 'article/article.html', context)
+
+def articleCreate(request):
+    template = 'article/articleCreateUpdate.html'
+    if request.method == 'GET':
+        return render(request, template, {'articleForm':ArticleForm()})
+    #POST
+    articleForm = ArticleForm(request.POST)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+    articleForm.save()
+    messages.success(request, '文章已新增')
+    return redirect('article:article')
+
+def articleRead(request,articleId):
+    article = get_object_or_404(Article, id=articleId)
+    context = {
+        'article': article,
+        'comments': Comment.objects.filter(article=article)
+    }
+    return render(request, 'article/articleRead.html', context)
+
+def articleUpdate(request, articleId):
+    article = get_object_or_404(Article, id=articleId)
+    template = 'article/articleCreateUpdate.html'
+    if request.method == 'GET':
+        articleForm = ArticleForm(instance=article)
+        return render(request, template, {'articleForm':articleForm})
+    #POST
+    articleForm = ArticleForm(request.POST, instance=article)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+    articleForm.save()
+    messages.success(request, '文章已修改')
+    return redirect('article:articleRead',articleId=articleId)
+
+def articleDelete(request, articleId):
+    if request.method == 'GET':
+        return article(request)
+    #POST
+    article = get_object_or_404(Article, id=articleId)
+    article.delete()
+    messages.success(request, '文章已刪除')
+    return redirect('article:article')
+
+def articleSearch(request):
+    searchTerm = request.GET.get('searchTerm')
+    articles = Article.objects.filter(Q(title__icontains=searchTerm) |
+                                      Q(content__icontains=searchTerm))
+    context = {'articles': articles, 'searchTerm': searchTerm}
+    return render(request, 'article/articleSearch.html', context)
