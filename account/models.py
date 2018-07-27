ce343f665045cb747a132c19a613afcf779ac9ec
ivan0chen <youngfu.chen@gmail.com>
2018-07-27 15:56:45 +0800

    Chapter 11 finished

---
+++ account/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    fullname = models.CharField(max_length=128)
+    website = models.URLField(blank=True, null=True)
+    address = models.CharField(max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return self.fullname + ' (' + self.username + ')'
